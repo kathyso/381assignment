cf0a1a332322cb87bb156a2e939e0557348e446b
kathyso <sokathy46@outlook.com>
2015-12-07 12:34:11 +0800

    delete

--- server.js
+++ server.js
@@ -17,16 +17,18 @@ app.post('/',function(req,res) {
 	db.once('open', function (callback) {
 		var rObj = {};
 		rObj.address = {};
-		rObj.address.building = req.body.building;
-		rObj.address.street = req.body.street;
-		rObj.address.zipcode = req.body.zipcode;
+		if (req.body.building) rObj.address.building = req.body.building;
+		if (req.body.street) rObj.address.street = req.body.street;
+		if (req.body.zipcode) rObj.address.zipcode = req.body.zipcode;
 		rObj.address.coord = [];
-		rObj.address.coord.push(req.body.lon);
-		rObj.address.coord.push(req.body.lat);
-		rObj.borough = req.body.borough;
-		rObj.cuisine = req.body.cuisine;
-		rObj.name = req.body.name;
-		rObj.restaurant_id = req.body.restaurant_id;
+		if (req.body.lon&&req.body.lat) {
+			rObj.address.coord.push(req.body.lon);
+			rObj.address.coord.push(req.body.lat);
+		}
+		if (req.body.borough) rObj.borough = req.body.borough;
+		if (req.body.cuisine) rObj.cuisine = req.body.cuisine;
+		if (req.body.name) rObj.name = req.body.name;
+		if (req.body.restaurant_id) rObj.restaurant_id = req.body.restaurant_id;
 
 		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
 		var r = new Restaurant(rObj);
@@ -117,8 +119,13 @@ app.delete('/grade/:attrib/:attrib_value',function(req,res) {
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function (callback) {
+		var rObj = {};
+		rObj.grades = {};
+		rObj.grades.date = req.body.date;
+		rObj.grades.grade = req.body.grade;
+		rObj.grades.score = req.body.score;
 		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
-		Restaurant.find(criteria).remove(function(err) {
+		Restaurant.find(criteria).remove(rObj,function(err) {
        		if (err) {
 				res.status(500).json(err);
 				throw err
